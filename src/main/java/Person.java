3271ddac71a89faf898ad565fe94e36a25902c73
Richsvon <RichardLejon@hotmail.com>
2021-04-13 11:56:07 +0200

    fix fails

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,42 +3,37 @@ public class Person {
     String name;
 
     public Person(int age, String name) {
-        if (age < 0){               // Alt 1: Om parametern age är mindre än noll, sätts åldern till noll
-            this.age =0;
-        }else {                     // Alt 2: Annars sätt åldern till den mottagna parametern age
+        if (age < 0) {               // Alt 1: Om parametern age är mindre än noll, sätts åldern till noll
             this.age = 0;
+        } else {                     // Alt 2: Annars sätt åldern till den mottagna parametern age
+            this.age = age;
         }
         this.name = name;           //Sätt namn till den mottagna parametern name
     }
 
     public int getAge() {           //Returnerar age
-        //return this.age;
-        return 0;
+        return this.age;
     }
 
     public void setAge(int age) {
-        if (age < 0){               // Alt 1: Om parametern age är mindre än noll, sätts åldern till noll
-            this.age =0;
-        }else {                     // Alt 2: Annars sätt åldern till den mottagna parametern age
-            //this.age = age;
+        if (age < 0) {               // Alt 1: Om parametern age är mindre än noll, sätts åldern till noll
             this.age = 0;
+        } else {                     // Alt 2: Annars sätt åldern till den mottagna parametern age
+            this.age = age;
         }
     }
 
     public String getName() {       //returnerar name
-        //return this.name;
-        return "";
+        return this.name;
     }
 
     public void setName(String name) {  //Sätt objektets name till den mottagna variablen
-        //this.name = name;
-        this.name = "";
+        this.name = name;
     }
 
-    public String reverseName(){
+    public String reverseName() {
         StringBuilder stringBuilder = new StringBuilder(this.name); //Skapar ett stringbuilder objekt av objektets name
         stringBuilder.reverse();                                    //Använder inbyggd metod från stringbuilder som spegelvänder name
-        //return stringBuilder.toString();                          // returnerar det spegelvända namnet med hjälp av toString()
-        return "";
+        return stringBuilder.toString();                          // returnerar det spegelvända namnet med hjälp av toString()
     }
 }
